3782b65d85cb55d166024633a6734aa825c1ddcb
yanghojoon <yanghojoon8487@gmail.com>
2023-10-19 10:52:12 +0900

    feat: 데이터 정렬 및 드래그 앤 드롭 기능 구현

--- URLSession+CollectionView/Main/ViewController.swift
+++ URLSession+CollectionView/Main/ViewController.swift
@@ -24,12 +24,13 @@ final class ViewController: UIViewController {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .systemGray6
         collectionView.showsHorizontalScrollIndicator = false
-        
+        collectionView.dragInteractionEnabled = true
         
         return collectionView
     }()
     private let networkManager = NetworkManager()
     private let disposeBag = DisposeBag()
+    private var images = [Image]()
     
     private var dataSource: UICollectionViewDiffableDataSource<Section, Image>?
     
@@ -44,7 +45,7 @@ final class ViewController: UIViewController {
     
     private func configureDataSource() {
         let cellRegistration = UICollectionView.CellRegistration<ImageCell, Image> { cell, indexPath, image in // register
-            cell.configure(item: image)
+//            cell.configure(item: image)
         }
         
         dataSource = UICollectionViewDiffableDataSource<Section, Image>( // cellForRowAt
@@ -55,6 +56,7 @@ final class ViewController: UIViewController {
                     for: indexPath,
                     item: image
                 )
+                cell.configure(item: image)
                 
                 return cell
         })
@@ -63,24 +65,25 @@ final class ViewController: UIViewController {
     private func fetchImageData(page: Int) {
         let api = ImageListAPI(page: page)
         
-//        NetworkManager().fetchData(
-//            api: ImageListAPI(page: 1),
-//            decodingType: [Image].self) { result in
-//                result.map { print($0) }
-//            }
-        
         NetworkManager().fetchData(api: api, decodingType: [Image].self)
             .withUnretained(self)
             .subscribe(onNext: { vc, image in
                 var snapshot = NSDiffableDataSourceSnapshot<Section, Image>() // 띄워주게 될 데이터
                 snapshot.appendSections([.basic])
                 snapshot.appendItems(image)
+                vc.images = image
                 vc.dataSource?.apply(snapshot, animatingDifferences: true)
             })
         .disposed(by: disposeBag)
     }
+    //        NetworkManager().fetchData(
+    //            api: ImageListAPI(page: 1),
+    //            decodingType: [Image].self) { result in
+    //                result.map { print($0) }
+    //            }
     
     private func render() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "정렬", style: .done, target: self, action: #selector(sortSnapshot))
         view.addSubview(collectionView)
         
         collectionView.snp.makeConstraints {
@@ -89,8 +92,21 @@ final class ViewController: UIViewController {
         }
     }
     
+    @objc
+    private func sortSnapshot() {
+        images = images.sorted(by: { $0.createdAt > $1.createdAt })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Image>()
+        snapshot.appendSections([.basic])
+        snapshot.appendItems(images)
+        
+        dataSource?.apply(snapshot, animatingDifferences: true)
+    }
+    
     private func setAttributes() {
         collectionView.delegate = self
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
     }
 }
 
@@ -101,5 +117,69 @@ extension ViewController: UICollectionViewDelegate, UICollectionViewDelegateFlow
         sizeForItemAt indexPath: IndexPath
     ) -> CGSize {
         CGSize(width: view.bounds.width, height: 100)
+    }
+}
+
+extension ViewController: UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+    func collectionView(_ collectionView: UICollectionView, itemsForBeginning session: UIDragSession, at indexPath: IndexPath) -> [UIDragItem] {
+        guard let item = dataSource?.itemIdentifier(for: indexPath) else {
+            return []
+        }
+        
+        let itemProvider = NSItemProvider(object: item.id as NSString)
+        let dragItem = UIDragItem(itemProvider: itemProvider)
+        dragItem.localObject = item
+        
+        return [dragItem]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, performDropWith coordinator: UICollectionViewDropCoordinator) {
+        var destination: IndexPath = IndexPath()
+        if let indexPatch = coordinator.destinationIndexPath {
+            destination = indexPatch
+        } else {
+            let item = collectionView.numberOfItems(inSection: .zero)
+            destination = IndexPath(item: item - 1, section: .zero)
+        }
+        
+        if coordinator.proposal.operation == .move {
+            editSnapshot(coordinator: coordinator, destination: destination, collectionView: collectionView)
+        }
+    }
+    
+    private func editSnapshot(
+        coordinator: UICollectionViewDropCoordinator,
+        destination: IndexPath,
+        collectionView: UICollectionView
+    ) {
+        if let item = coordinator.items.first,
+           let origin = item.sourceIndexPath,
+           let fromItem = dataSource?.itemIdentifier(for: origin),
+           let toItem = dataSource?.itemIdentifier(for: destination),
+           var snapshot = dataSource?.snapshot() {
+            snapshot.deleteItems([fromItem])
+            
+            if destination.row > origin.row {
+                snapshot.insertItems([fromItem], afterItem: toItem)
+            } else {
+                snapshot.insertItems([fromItem], beforeItem: toItem)
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.dataSource?.apply(snapshot)
+            }
+        }
+    }
+    
+    func collectionView(
+        _ collectionView: UICollectionView,
+        dropSessionDidUpdate session: UIDropSession,
+        withDestinationIndexPath destinationIndexPath: IndexPath?
+    ) -> UICollectionViewDropProposal {
+        if collectionView.hasActiveDrag {
+            return UICollectionViewDropProposal(operation: .move, intent: .insertAtDestinationIndexPath)
+        }
+        
+        return UICollectionViewDropProposal(operation: .forbidden)
     }
 }
